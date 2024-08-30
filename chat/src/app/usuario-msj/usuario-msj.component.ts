981d36b9b2e2edbf5f6e5b7ce30770a6da62cffb
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-08-30 19:51:42 -0300

    cosas

--- chat/src/app/usuario-msj/usuario-msj.component.ts
+++ chat/src/app/usuario-msj/usuario-msj.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-usuario-msj',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usuario-msj.component.html',
   styleUrl: './usuario-msj.component.css'
 })
 export class UsuarioMsjComponent {
-
+  @Input() nombre : string = ""
+  mensaje : string = ""
 }
